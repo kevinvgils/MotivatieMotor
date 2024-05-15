f9574c6afd737ce900ae5478a78ca23d923170da
Wouter-Klaassen <94183945+Wouter-Klaassen@users.noreply.github.com>
2024-05-15 13:16:04 +0200

    Added UI assets

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+
+
+    //Quits game
+    void Quit()
+    {
+
+    }
+}
