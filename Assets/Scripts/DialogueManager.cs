8fecc2eb2b522b637c2f95e66d8fb3fb7a7940eb
Kevin van Gils <kvnvangils@gmail.com>
2024-05-20 12:09:33 +0200

    end sentence QoL

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +13,8 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text dialogueText;
 
     public Animator animator;
+    private bool IsCoroutineRunning;
+    private Sentence currentSentence;
 
     // Start is called before the first frame update
     void Start()
@@ -37,17 +40,24 @@ public class DialogueManager : MonoBehaviour
     }
 
     public void DisplayNextSentence() {
+        if(IsCoroutineRunning) {
+            StopAllCoroutines();
+            IsCoroutineRunning = false;
+            dialogueText.text = currentSentence.sentence;
+            return;
+        }
         if (sentences.Count == 0) {
             EndDialogue();
             return;
         }
 
-        Sentence sentence = sentences.Dequeue();
+        currentSentence = sentences.Dequeue();
         StopAllCoroutines();
-        StartCoroutine(TypeSentence(sentence));
+        StartCoroutine(TypeSentence(currentSentence));
     }
 
     IEnumerator TypeSentence(Sentence sentence) {
+        IsCoroutineRunning = true;
         dialogueText.text = "";
         nameText.text = sentence.name;
         foreach(char letter in sentence.sentence.ToCharArray()) {
